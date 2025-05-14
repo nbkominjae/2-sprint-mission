b5430731ad5a44f8847dcfbd516685e6efe018c3
nbkominjae <horo1008@naver.com>
2025-05-14 11:07:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -41,3 +41,23 @@ class Article{
     }
 }
 
+// getProductList()를 통해서 받아온 상품 리스트를 각각 인스턴스로 만들어  products 배열에 저장해 주세요.
+//해시태그에 "전자제품"이 포함되어 있는 상품들은 Product 클래스 대신 ElectronicProduct 클래스를 사용해 인스턴스를 생성해 주세요.
+
+
+let products = [];
+
+const data = await getProductList();
+data.list.forEach(item => {
+    if(item.tags.includes('전자제품')){
+        const electroproduct = new ElectronicProduct (item.name,item.description,item.price,item.tags,item.images,item.favoriteCount,item.manufacturer);
+        products.push(electroproduct)
+
+    }
+    else {
+        const product = new Product(item.name,item.description,item.price,item.tags,item.images,item.favoriteCount);
+        products.push(product);
+    }
+
+});
+
