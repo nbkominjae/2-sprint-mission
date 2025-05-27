70dde63250c7e3b77ba70ffc7466a7d059255adc
Your Name <youremail@example.com>
2025-05-27 13:16:47 +0900

    article,product crud

---
+++ routes/article.js
@@ -0,0 +1,103 @@
+var express = require('express');
+const { db } = require ('../utils/db');
+var router = express.Router();
+
+
+// 게시글 상세 조회 API
+
+router.get('/detail/:id' , async function(req,res,next){
+  try{
+  const id = Number(req.params.id);
+  const detailArticle = await db.article.findUnique({
+    where : {id : id}
+  });
+  if(!detailArticle){
+    res.status(404).json({message : '없는 게시물입니다.'})
+    return;
+  }
+  res.json(detailArticle);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
+});
+
+// 게시글 목록 조회 API 
+
+router.get('/list', async function(req,res,next){
+  try{
+  const { title = '' , content = ''} = req.query; 
+  const article = await db.article.findMany({
+    where: {
+      OR : [
+        {title : {contains : title , mode : 'insensitive'}},
+        {content : {contains : content , mode : 'insensitive'}}
+      ]
+    },
+    select : {
+      id : true,
+      title : true,
+      content : true,
+      createdAt : true
+    },
+    orderBy : {createdAt : 'desc'},
+    skip : 0 ,
+    take : 10
+  });
+  res.json(article);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
+});
+
+
+// 게시물 등록 API 
+router.post('/register', async function (req,res,next) {
+  try{
+  const {title , content} = req.body;
+  const article = await db.article.create ({
+    data : {title , content}
+  });
+  res.json(article);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
+});
+
+
+// 게시글 수정 API
+
+router.patch('/change/:id', async function(req,res,next){
+  try{
+  const id = Number(req.params.id);
+  const data = req.body;
+  const updatedArticle = await db.article.update({
+    where : { id : id},
+    data: {
+      ...data
+    }
+  });
+  res.json(updatedArticle);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
+});
+
+// 게시글 삭제 API
+
+router.delete('/remove/:id', async function(req,res,next){
+  try{
+  const id = Number(req.params.id);
+  const deleteArticle = await db.article.delete({
+    where:{id : id}
+  })
+  res.json(deleteArticle);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
+});
+
--- routes/product.js
+++ routes/product.js
@@ -4,40 +4,107 @@ var router = express.Router();
 
 
 // 상품 상세조회 API
-router.get('/list/:id', async function(req,res,next){
+
+router.get('/detail/:id', async function(req,res,next){
+  try{
   const id = Number(req.params.id)
   const product = await db.product.findUnique({
     where: {id : id}
-  });
+  })
+  if(!product){
+    return res.status(404).json({ message : '없는 상품입니다.'})
+  }
   res.json(product);
+
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
+  
+});
+
+// 상품 목록 조회 API 
+
+router.get('/list', async function (req,res,next) {
+  try{
+  const {name = '' , description = ''} = req.query;
+
+  const product = await db.product.findMany({
+    where:{
+      OR: [
+        {name : {contains : name  , mode : 'insensitive'}},
+        {description : {contains : description, mode : 'insensitive'}}
+      ]    
+    },
+    select : {
+      id : true,
+      name : true,
+      price : true,
+      createdAt : true
+    },
+    orderBy : {createdAt : 'desc' },
+    skip : 0,
+    take : 10
+  })
+  res.send(product);
+
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
 });
 
 
 // 상품 등록 API
 
 router.post('/create', async function(req,res,next) {
+  try{
   const {name, description, price, tags} = req.body;
   
   const product = await db.product.create({
     data : {name, description, price, tags}
   })
-  res.json({id : product.id})
+  res.json({product})
+
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
 });
 
 //  상품 수정 API 
 
-router.patch('/product/:id', async function (req,res,next ) {
+router.patch('/change/:id', async function (req,res,next ) {
+  try{
   const id = Number(req.params.id);
   const data = req.body;
-  
-  const change = {
-    data.id : 312,
-    data.name : 'sdas',
- 
+  const updatedData = await db.product.update({
+    where: { id : id},
+    data : {
+      ...data
+    }
+  })
+  res.json(updatedData);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
   }
-  
-})
+});
 
+// 상품 삭제 API 
+
+router.delete('/remove/:id', async function (req,res,next){
+  try{
+  const id = Number(req.params.id);
+  const deleteProduct = await db.product.deleteMany({
+    where : {id : id}
+  })
+  res.json(deleteProduct);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message :'서버에러'});
+  }
+}), 
 
 module.exports = router;
 
